191eb38e3c271cd30e45ecdebcfa0bb050add1be
Leonardo Nacagawa Silva <99892109+nakaleo13@users.noreply.github.com>
2022-06-01 23:04:41 -0300

    Add files via upload

---
+++ Banco-de-dados/Script final do projeto individual.sql
@@ -0,0 +1,29 @@
+create database projeto_individual;
+
+use projeto_individual;
+
+create table usuarios(
+idUsuario int primary key auto_increment,
+nomeUsuario varchar(50),
+emailUsuario varchar(80),
+senhaUsuario varchar(50));
+
+create table resultado_quiz(
+idResultado int auto_increment,
+fk_usuario int,
+foreign key (fk_usuario) references usuarios (idUsuario),
+primary key (idResultado,fk_usuario),
+qtd_acertos int,
+data_hora datetime default current_timestamp);
+
+select * from usuarios;
+
+select * from resultado_quiz;
+
+select round(avg(qtd_acertos),2) from resultado_quiz;
+
+select qtd_acertos from resultado_quiz;
+
+
+select qtd_acertos quantidadeAcertos,count(qtd_acertos) quantidadeJogadas from resultado_quiz group by qtd_acertos;
+
